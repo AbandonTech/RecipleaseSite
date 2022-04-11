4db2dd9eb9b90edc387e0f0dda9d7464bbdf8af7
fisher60 <wakefollower@gmail.com>
2022-04-10 19:05:51 -0500

    configure tailwind, add example components, add config file

---
+++ site/src/components/listRecipes/ListRecipes.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class ListRecipes extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                List Recipes Component
+            </div>
+        );
+    }
+}
+ 
+export default ListRecipes;
---
+++ site/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+
+class Navbar extends Component {
+    state = {  } 
+    render() { 
+        return <div className="bg-teal-500">Reciplease Navbar</div>;
+    }
+}
+ 
+export default Navbar;
